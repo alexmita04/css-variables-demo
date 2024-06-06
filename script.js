c4bc90e42ba78d7e5024071cd2f867bdb5beea30
alexmita04 <alexmita04@gmail.com>
2024-06-06 13:52:30 +0300

    added colorInput functionality

--- script.js
+++ script.js
@@ -6,5 +6,19 @@ const img = document.querySelector("img");
 
 blurInput.addEventListener("input", (e) => {
   const blurValue = e.target.value + "px";
-  document.documentElement.style.setProperty(`--blur`, blurValue);
+  document.documentElement.style.setProperty(`--${e.target.name}`, blurValue);
+});
+
+spacingInput.addEventListener("input", (e) => {
+  const spacingValue = e.target.value + "px";
+  document.documentElement.style.setProperty(
+    `--${e.target.name}`,
+    spacingValue
+  );
+});
+
+colorInput.addEventListener("input", (e) => {
+  const colorValue = e.target.value;
+  console.log(colorValue);
+  document.documentElement.style.setProperty(`--base`, colorValue);
 });
