8d1bf25e44a7fc0308ee49f68247ee36faf71c6e
alexmita04 <alexmita04@gmail.com>
2024-06-06 13:40:54 +0300

    added blur functionality

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const blurInput = document.getElementById("blur");
 const colorInput = document.getElementById("color");
 const imgContainer = document.querySelector(".image-container");
 const img = document.querySelector("img");
+
+blurInput.addEventListener("input", (e) => {
+  const blurValue = e.target.value + "px";
+  document.documentElement.style.setProperty(`--blur`, blurValue);
+});
