e2a5e4bce57e38590009e5f7725480f25116150b
alexmita04 <alexmita04@gmail.com>
2024-06-06 13:28:18 +0300

    declared the html elements in js

---
+++ script.js
@@ -0,0 +1,5 @@
+const spacingInput = document.getElementById("spacing");
+const blurInput = document.getElementById("blur");
+const colorInput = document.getElementById("color");
+const imgContainer = document.querySelector(".image-container");
+const img = document.querySelector("img");
